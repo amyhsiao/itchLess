aea1b1016445fd848501c5fb48dc614131d24703
Amy Hsiao <amyhsiao2015@gmail.com>
2024-07-07 19:58:29 +0800

    Initial Commit

---
+++ itchLess/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  itchLess
+//
+//  Created by Amy Hsiao on 2024/7/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ itchLess/itchLessApp.swift
@@ -0,0 +1,17 @@
+//
+//  itchLessApp.swift
+//  itchLess
+//
+//  Created by Amy Hsiao on 2024/7/5.
+//
+
+import SwiftUI
+
+@main
+struct itchLessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
